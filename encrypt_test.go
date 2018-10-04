ce67238011ca3135f8aa1478eb7217de668dbaec
Sergio Andres Virviescas Santana <developersavsgio@gmail.com>
2018-10-04 15:44:03 +0200

    Fix typo

--- encrypt_test.go
+++ encrypt_test.go
@@ -6,64 +6,64 @@ import (
 
 func BenchmarkGOBEncode(b *testing.B) {
 	e := NewEncrypt()
-	d := new(Dict)
+	src := new(Dict)
 
-	d.Set("k1", 1)
-	d.Set("k2", 2)
-	d.Set("k3", 3)
-	d.Set("k4", 4)
+	src.Set("k1", 1)
+	src.Set("k2", 2)
+	src.Set("k3", 3)
+	src.Set("k4", 4)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		e.GOBEncode(d)
+		e.GOBEncode(src)
 	}
 }
 
 func BenchmarkGOBDecode(b *testing.B) {
 	e := NewEncrypt()
-	d := new(Dict)
+	src := new(Dict)
 
-	d.Set("k1", 1)
-	d.Set("k2", 2)
-	d.Set("k3", 3)
-	d.Set("k4", 4)
+	src.Set("k1", 1)
+	src.Set("k2", 2)
+	src.Set("k3", 3)
+	src.Set("k4", 4)
 
-	data, _ := e.GOBEncode(d)
+	srcBytes, _ := e.GOBEncode(src)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		e.GOBDecode(data)
+		e.GOBDecode(srcBytes)
 	}
 }
 
 func BenchmarkBase64Encode(b *testing.B) {
 	e := NewEncrypt()
-	d := new(Dict)
+	src := new(Dict)
 
-	d.Set("k1", 1)
-	d.Set("k2", 2)
-	d.Set("k3", 3)
-	d.Set("k4", 4)
+	src.Set("k1", 1)
+	src.Set("k2", 2)
+	src.Set("k3", 3)
+	src.Set("k4", 4)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		e.Base64Encode(d)
+		e.Base64Encode(src)
 	}
 }
 
 func BenchmarkBase64Decode(b *testing.B) {
 	e := NewEncrypt()
-	d := new(Dict)
+	src := new(Dict)
 
-	d.Set("k1", 1)
-	d.Set("k2", 2)
-	d.Set("k3", 3)
-	d.Set("k4", 4)
+	src.Set("k1", 1)
+	src.Set("k2", 2)
+	src.Set("k3", 3)
+	src.Set("k4", 4)
 
-	data, _ := e.Base64Encode(d)
+	srcBytes, _ := e.Base64Encode(src)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		e.Base64Decode(data)
+		e.Base64Decode(srcBytes)
 	}
 }
