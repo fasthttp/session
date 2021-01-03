6d6146b274e77e2ce2f4b9582d876151f5b0068a
James Elliott <james-d-elliott@users.noreply.github.com>
2021-01-04 10:24:35 +1100

    [BUGFIX] Make Session Attribute Key Non Random
    * between v1 and v2 the session expiration attr key became a random value
    * this caused sessions generated in one app/daemon/etc to have expirations not aligning with others
    * this patch basically restores the functionality found in v1
    * fixes #32

--- const.go
+++ const.go
@@ -14,3 +14,4 @@ const defaultCookieLen uint32 = 32
 // If set the cookie expiration when the browser is closed (-1), set the expiration as a keep alive (2 days)
 // so as not to keep dead sessions for a long time
 const keepAliveExpiration = 2 * 24 * time.Hour
+const expirationAttrKey = "__store:expiration__"
