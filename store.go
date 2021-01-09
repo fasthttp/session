1423b97542998ae1fc1510afa412a284ddfea18e
James Elliott <james-d-elliott@users.noreply.github.com>
2021-01-10 02:21:07 +1100

    [BUGFIX] Make Session Attribute Key Non Random (#33)
    * between v1 and v2 the session expiration attr key became a random value
    * this caused sessions generated in one app/daemon/etc to have expirations not aligning with others
    * this patch basically restores the functionality found in v1
    * fixes #32

--- store.go
+++ store.go
@@ -1,13 +1,8 @@
 package session
 
 import (
-	"fmt"
 	"time"
-
-	"github.com/savsgio/gotils/bytes"
 )
-
-var expirationAttrKey = fmt.Sprintf("__store:expiration:%s__", bytes.Rand(make([]byte, 5)))
 
 // NewStore returns a new empty store
 func NewStore() *Store {
